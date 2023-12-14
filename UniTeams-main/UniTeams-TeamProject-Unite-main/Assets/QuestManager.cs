a2f493eec242e78250c81a5f1f15a2e06f41972e
kdg0320 <kdg010320@naver.com>
2023-12-15 04:54:40 +0900

    퀘스트매니저, 퀘스트UI추가
    몬스터 사망시 퀘스트매니저의 관련변수를 증가시켜주면 다음 퀘스트로 넘어간다.

--- UniTeams-main/UniTeams-TeamProject-Unite-main/Assets/QuestManager.cs
+++ UniTeams-main/UniTeams-TeamProject-Unite-main/Assets/QuestManager.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     public int FirstMonsterKill = 0;
+    public int MiddleBossMonsterKill = 0;
     public int SecondMonsterKill = 0;
+    public GameObject Q1txt;
+    public GameObject Q2txt;
+    public GameObject Q3txt;
+    public GameObject Q4txt;
 
     // Update is called once per frame
     void Update()
@@ -14,11 +19,22 @@ public class QuestManager : MonoBehaviour
         {
             FirstMonsterKill = -1;
             Debug.Log("10마리 처치 완료");
+            Q1txt.SetActive(false);
+            Q2txt.SetActive(true);
+        }
+        if (MiddleBossMonsterKill == 1)
+        {
+            MiddleBossMonsterKill = -1;
+            Debug.Log("중간보스 처치 완료");
+            Q2txt.SetActive(false);
+            Q3txt.SetActive(true);
         }
         if (SecondMonsterKill >= 15)
         {
             SecondMonsterKill = -1;
             Debug.Log("15마리 처치 완료");
+            Q3txt.SetActive(false);
+            Q4txt.SetActive(true);
         }
     }
 }
